a9acefaa306bce8edc9d01dfacbdf6dcb060fd1c
Сергей <serega16.92@inbox.ru>
2023-03-31 09:08:36 +0500

    Надо подкачаться

--- js/api.js
+++ js/api.js
@@ -4,17 +4,12 @@ const Route = {
   SEND_DATA: '/',
 };
 
-const Method = {
-  GET: 'GET',
-  POST: 'POST',
-};
-
 const ErrorText = {
   GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
   SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
 };
 
-const load = (route, errorText, method = Method.GET, body = null) =>
+const load = (route, errorText, method, body = null) =>
   fetch(`${BASE_URL}${route}`, {method, body})
     .then((response) => {
       if(!response.ok) {
@@ -27,8 +22,8 @@ const load = (route, errorText, method = Method.GET, body = null) =>
     });
 
 
-const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA);
+const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA, 'GET');
 
-const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, 'POST', body);
 
 export {getData, sendData};
